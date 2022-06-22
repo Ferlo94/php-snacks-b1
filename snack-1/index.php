71bb17e0f67394124752c2982bb72416345810dd
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-22 16:22:14 +0200

    p?

--- snack-1/index.php
+++ snack-1/index.php
@@ -24,7 +24,7 @@ $partite = [
 
 for ($i=0; $i < count($partite); $i++) { 
 
-    echo "{$partite[$i]['squadraCasa']} {$partite[$i]['squadraOspite']} {$partite[$i]['puntiCasa']} {$partite[$i]['puntiOspite']}";
+    echo "{$partite[$i]['squadraCasa']} <p> - </p> {$partite[$i]['squadraOspite']} {$partite[$i]['puntiCasa']} {$partite[$i]['puntiOspite']}";
 }
 
 
