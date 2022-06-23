74964be43ca7d6ec6bb67174d8930f0c690591ed
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-23 14:56:13 +0200

    aggiornamento esercizio snack 2

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,43 +1,13 @@
 <?php
-    $_GET['name'];
-    $_GET['email'];
-    $_GET['age'];
+
+
+    if (strlen($_GET['name']) > 3 && filter_var($_GET['email'], FILTER_VALIDATE_EMAIL) && is_numeric($_GET['age'])) {
+        echo "ACCESSOAUTORIZZATO";
+    } else {
+        echo "ACCESSO NEGATO";
+    }
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 
-    <!-- 
 
-    Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
-    che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
-    Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
-
-    -->
-
-    <?php 
-
-        if(strlen('name') > 3){
-
-            if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-
-                if(filter_var($age,FILTER_VALIDATE_INT)){
-                    echo "$email ACCESSO RIUSCITO";     
-                }
-            }
-            
-            else{
-                echo "$email ACCESSO NEGATO";
-            }
-        }
-    ?>
-
-</body>
-</html>
